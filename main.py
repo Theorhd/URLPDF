91e76f3c5f45230cc8a30a33c45fda0d73ea8634
Theo <theo.richard8654@gmail.com>
2025-03-13 17:13:33 +0100

    Récupération des URLS de MammouthAI

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from PDFGenerator.PDFGenerator import PDFGenerator
 
 if __name__ == "__main__":
     pdf = PDFGenerator(output_dir="output")
-    urls = pdf.all_urls("https://docs.crewai.com", 1000, False)
+    urls = pdf.all_urls("https://mammouth.ai/", 1000, False)
     pdf.generate_pdfs_from_urls(urls, False, False)
